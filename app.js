5605789b976957fcd65b594fb1eb2db587965170
Aziza Nurlayeva <itpark9876@gmail.com>
2026-03-11 17:40:34 +0500

    learned number and number methods

---
+++ app.js
@@ -0,0 +1,29 @@
+// numbers and number methods
+let num = 1_000_000;
+// rounding - yaxlitlash
+let myNum = 4.9
+console.log(Math.floor(myNum)); // pastga qarab yaxlitlash 
+
+console.log(Math.round(myNum)); // normal yaxlitlash
+
+console.log(Math.ceil(myNum)); // tepaga qarab yaxlitlash
+
+console.log(Math.trunc(myNum)); // nuqatadan keyingi sonlarni tashlab yuborish
+
+let newNum2 = 12.3456789 // yaxlitlaydi va qirqib oladi
+console.log(newNum2.toFixed(4));
+
+// NaN - Not a Number
+// isNaN - son emasmi
+console.log(NaN === NaN); // string, 1n, true, false, null, undefined, object, array, function
+console.log(isFinite('100rem')); // butun songa ogirish
+console.log(parseFloat('12.5em')); // qoldiqli | haqiqiy songa ogiradi
+
+
+
+
+
+
+
+
+
